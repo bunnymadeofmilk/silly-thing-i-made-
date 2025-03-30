892ce5ecfbde66f41a7e1d2d772483ecd5184b90
sasha <hazy112@protonmail.com>
2025-03-29 20:26:06 -0700

    Add files via upload

---
+++ silly.py
@@ -0,0 +1,21 @@
+print("hello there!!!") 
+gender = input ("are you a boy or are you a girl???: ")
+
+if gender == "bunny":
+    print ("holy fucking bunny")
+
+if gender == "boy":
+    print("aww what a good boy")
+    animal = input("are you kitty or are you puppy???: ")
+    if animal == "kitty":
+        print("awww good catboy *pets you uwu*")
+    if animal == "puppy":
+        print("awww *pets da puppyboy*")    
+
+if gender == "girl":    
+    print ("awww what a good girl :3")
+    animal = input ("are you a kitty or puppy??: " )
+    if animal == "kitty":
+        print ('pets the kittygirl nya~')
+    if animal == 'puppy':
+        print("pets da ouppy girl :3")
